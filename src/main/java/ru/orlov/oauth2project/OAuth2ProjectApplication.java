adeff154c4e69fc9c6ef3877040c7c4a2287bc89
Dima <ddd12@mail.ru>
2023-06-15 14:12:39 +0300

    first commit

---
+++ src/main/java/ru/orlov/oauth2project/OAuth2ProjectApplication.java
@@ -0,0 +1,11 @@
+package ru.orlov.oauth2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/orlov/oauth2project/OAuth2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.orlov.oauth2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
