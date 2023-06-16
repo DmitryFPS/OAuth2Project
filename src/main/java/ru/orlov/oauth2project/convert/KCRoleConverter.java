796c795ff959a8027cedc0e8d3e646aba4c37fde
Dima <ddd12@mail.ru>
2023-06-16 10:15:28 +0300

    c4

---
+++ src/main/java/ru/orlov/oauth2project/convert/KCRoleConverter.java
@@ -0,0 +1,33 @@
+package ru.orlov.oauth2project.convert;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KCRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(final Jwt source) {
+        final Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        final List<GrantedAuthority> grantedAuthorities = ((List<String>) realmAccess.get("roles")).stream()
+                .map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        System.out.println(grantedAuthorities.size());
+        System.out.println(grantedAuthorities);
+
+        return grantedAuthorities;
+    }
+}
--- src/main/java/ru/orlov/oauth2project/security/SpringSecurityConfig.java
+++ src/main/java/ru/orlov/oauth2project/security/SpringSecurityConfig.java
@@ -4,16 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.orlov.oauth2project.convert.KCRoleConverter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KCRoleConverter());
+
         http
-                .authorizeHttpRequests(auth -> auth.antMatchers("/api/test/login").permitAll())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+                .authorizeRequests().antMatchers("/api/test/login").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+
         return http.build();
     }
 }
